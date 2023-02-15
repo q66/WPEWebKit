505c5463f3bdec3d16efab6f5e32b4c8be05de2d
Przemyslaw Gorszkowski <pgorszkowski@igalia.com>
2023-02-15 10:40:16 +0100

    Jettison on top level navigation

--- Source/WebCore/loader/FrameLoader.cpp
+++ Source/WebCore/loader/FrameLoader.cpp
@@ -83,6 +83,7 @@
 #include "IgnoreOpensDuringUnloadCountIncrementer.h"
 #include "InspectorController.h"
 #include "InspectorInstrumentation.h"
+#include "LegacySchemeRegistry.h"
 #include "LinkLoader.h"
 #include "LoaderStrategy.h"
 #include "LocalDOMWindow.h"
@@ -2103,7 +2104,8 @@ void FrameLoader::commitProvisionalLoad()
         // We are doing this here because we know for sure that a new page is about to be loaded.
         BackForwardCache::singleton().addIfCacheable(*history().currentItem(), m_frame.page());
         
-        WebCore::jettisonExpensiveObjectsOnTopLevelNavigation();
+        if (pdl && LegacySchemeRegistry::shouldLoadURLSchemeAsEmptyDocument(pdl->request().url().protocol().toStringWithoutCopying()))
+            WebCore::jettisonExpensiveObjectsOnTopLevelNavigation();
     }
 
     if (m_loadType != FrameLoadType::Replace)

932e27e4e84d81c7c40fc700b943843d43c1a4aa
Miguel Gomez <magomez@igalia.com>
2023-03-09 16:23:14 +0100

    Take into account scaling operations when deciding backingStore scale factor

--- Source/WebCore/platform/graphics/texmap/coordinated/CoordinatedGraphicsLayer.h
+++ Source/WebCore/platform/graphics/texmap/coordinated/CoordinatedGraphicsLayer.h
@@ -230,6 +230,9 @@ private:
     RefPtr<BitmapTexture> acquireTextureForAcceleratedBuffer(const IntSize&);
 #endif
 
+    void updateAnimationScaleFactor();
+    void updateAnimationOrTransformScaleFactor();
+
     Nicosia::PlatformLayer::LayerID m_id;
     GraphicsLayerTransform m_layerTransform;
     TransformationMatrix m_cachedInverseTransform;
@@ -279,6 +282,9 @@ private:
 
     RefPtr<AnimatedBackingStoreHost> m_animatedBackingStoreHost;
     RefPtr<CoordinatedGraphicsLayer> m_backdropLayer;
+
+    float m_animationScaleFactor { 1.0 };
+    float m_animationOrTransformScaleFactor { 1.0 };
 };
 
 } // namespace WebCore

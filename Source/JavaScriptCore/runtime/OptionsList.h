b77f8229a325f3a2e01ed1dcc07ad6d1617defd2
Miguel Gomez <magomez@igalia.com>
2023-08-31 19:17:10 +0200

    [JSC] Add option to disable console logging

--- Source/JavaScriptCore/runtime/OptionsList.h
+++ Source/JavaScriptCore/runtime/OptionsList.h
@@ -605,7 +605,7 @@ bool hasCapacityToUseLargeGigacage();
     v(Bool, useWebAssemblyRelaxedSIMD, false, Normal, "Allow the relaxed simd instructions and types from the wasm relaxed simd spec.") \
     v(Bool, useWebAssemblyTailCalls, false, Normal, "Allow the new instructions from the wasm tail calls spec.") \
     v(Bool, useWebAssemblyExtendedConstantExpressions, true, Normal, "Allow the use of global, element, and data init expressions from the extended constant expressions proposal.") \
-
+    v(Bool, disableConsoleLog, false, Normal, "Disable printing of JS console logs.") \
 
 
 enum OptionEquivalence {

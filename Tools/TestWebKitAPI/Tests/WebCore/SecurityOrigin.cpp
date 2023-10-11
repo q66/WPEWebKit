001b0490199ae28596ec5791487ea437e8f11f12
Miguel Gomez <magomez@igalia.com>
2023-10-11 17:45:19 +0200

    Revert a209cbf453978e3ea809d36f684a36f36ff301c5 as it breaks retrieving blobs.

--- Tools/TestWebKitAPI/Tests/WebCore/SecurityOrigin.cpp
+++ Tools/TestWebKitAPI/Tests/WebCore/SecurityOrigin.cpp
@@ -321,10 +321,4 @@ TEST_F(SecurityOriginTest, SecurityOriginHash)
     EXPECT_TRUE(set.remove(o2->data()));
     EXPECT_FALSE(set.contains(o1->data()));
 }
-
-TEST_F(SecurityOriginTest, IsSecureBlobURL)
-{
-    EXPECT_FALSE(SecurityOrigin::isSecure(URL { "blob:"_str }));
-}
-
 } // namespace TestWebKitAPI

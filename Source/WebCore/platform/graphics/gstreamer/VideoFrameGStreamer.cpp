bc510e647b4077e9f10aecd2c234080e99cef33c
Xabier Rodriguez-Calvar <calvaris@igalia.com>
2024-02-26 07:46:36 -0800

    [GStreamer] Make GstMappedFrame assert when it is not initialized https://bugs.webkit.org/show_bug.cgi?id=269980
    Reviewed by Carlos Garcia Campos.
    
    This way we can align the implementation of GstMappedFrame with GstMappedBuffer and assert when its data is accessed but
    it was not properly mapped at initialization.
    
    Even when now it is properly protected at members access, additional checks were added in some places where its
    instantiation was not being checked.
    
    A fly-by improvement is making the constructor taking GRefPtr<GstSample> to take const & to avoid unnecessary ref/unref.
    
    * Source/WebCore/platform/graphics/gstreamer/GStreamerCommon.h:
    (WebCore::GstMappedFrame::GstMappedFrame):
    (WebCore::GstMappedFrame::get):
    (WebCore::GstMappedFrame::ComponentData const):
    (WebCore::GstMappedFrame::ComponentStride const):
    (WebCore::GstMappedFrame::info):
    (WebCore::GstMappedFrame::width const):
    (WebCore::GstMappedFrame::height const):
    (WebCore::GstMappedFrame::format const):
    (WebCore::GstMappedFrame::planeData const):
    (WebCore::GstMappedFrame::planeStride const):
    (WebCore::GstMappedFrame::isValid const):
    (WebCore::GstMappedFrame::operator! const):
    * Source/WebCore/platform/graphics/gstreamer/VideoFrameGStreamer.cpp:
    (WebCore::VideoFrame::copyTo):
    * Source/WebCore/platform/mediastream/libwebrtc/gstreamer/GStreamerVideoFrameLibWebRTC.cpp:
    (WebCore::GStreamerVideoFrameLibWebRTC::ToI420):
    
    Canonical link: https://commits.webkit.org/275320@main

--- Source/WebCore/platform/graphics/gstreamer/VideoFrameGStreamer.cpp
+++ Source/WebCore/platform/graphics/gstreamer/VideoFrameGStreamer.cpp
@@ -363,7 +363,13 @@ void VideoFrame::copyTo(std::span<uint8_t> destination, VideoPixelFormat pixelFo
     auto* inputBuffer = gst_sample_get_buffer(sample);
     auto* inputCaps = gst_sample_get_caps(sample);
     gst_video_info_from_caps(&inputInfo, inputCaps);
-    GstMappedFrame inputFrame(inputBuffer, inputInfo, GST_MAP_READ);
+    GstMappedFrame inputFrame(inputBuffer, &inputInfo, GST_MAP_READ);
+    if (!inputFrame) {
+        GST_WARNING("could not map the input frame");
+        ASSERT_NOT_REACHED_WITH_MESSAGE("could not map the input frame");
+        callback({ });
+        return;
+    }
 
     GST_TRACE("Copying frame data to pixel format %d", static_cast<int>(pixelFormat));
     if (pixelFormat == VideoPixelFormat::NV12) {

WebKit/WebKit

[GTK][WPE] missing URI for injected user scripts and style sheets https://bugs.webkit.org/show_bug.cgi?id=272152
Reviewed by Adrian Perez de Castro.

User scripts/style sheets without the URI are not visible in WebInspector.

With this change the URI of injected user scripts/style sheets are generated as:
user-script:incremented_identifier
user-style:incremented_identifier

* LayoutTests/userscripts/window-onerror-for-isolated-world-1-expected.txt:
* LayoutTests/userscripts/window-onerror-for-isolated-world-2-expected.txt:
* LayoutTests/userscripts/window-onerror-for-isolated-world-3-expected.txt:
* Source/WebCore/Sources.txt:
* Source/WebCore/WebCore.xcodeproj/project.pbxproj:
* Source/WebCore/page/UserScript.cpp: Copied from Source/WebKit/UIProcess/API/APIUserScript.cpp.
(WebCore::generateUniqueURL):
(WebCore::UserScript::UserScript):
* Source/WebCore/page/UserScript.h:
(WebCore::UserScript::UserScript): Deleted.
* Source/WebCore/page/UserStyleSheet.cpp: Copied from Source/WebKit/UIProcess/API/APIUserScript.cpp.
(WebCore::generateUniqueURL):
(WebCore::UserStyleSheet::UserStyleSheet):
* Source/WebCore/page/UserStyleSheet.h:
* Source/WebCore/page/WorkerNavigator.cpp:
* Source/WebKit/UIProcess/API/APIUserScript.cpp:
(API::UserScript::generateUniqueURL): Deleted.
* Source/WebKit/UIProcess/API/APIUserScript.h:
* Source/WebKit/UIProcess/API/APIUserStyleSheet.cpp:
(API::UserStyleSheet::generateUniqueURL): Deleted.
* Source/WebKit/UIProcess/API/APIUserStyleSheet.h:
* Source/WebKit/UIProcess/API/C/WKUserScriptRef.cpp:
(WKUserScriptCreateWithSource):

Canonical link: https://commits.webkit.org/277470@main

Source/WebCore/page/UserScript.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Apple Inc. All rights reserved.
+ * Copyright (C) 2024 Igalia S.L. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -23,34 +23,28 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#pragma once
+#include "config.h"
+#include "UserScript.h"
 
-#include "APIContentWorld.h"
-#include "APIObject.h"
-#include "UserScriptIdentifier.h"
-#include <WebCore/UserScript.h>
-#include <wtf/Identified.h>
+#include <wtf/text/StringConcatenateNumbers.h>
 
-namespace API {
+namespace WebCore {
 
-class UserScript final : public ObjectImpl<Object::Type::UserScript>, public Identified<WebKit::UserScriptIdentifier> {
-public:
-    static Ref<UserScript> create(WebCore::UserScript&& userScript, API::ContentWorld& world)
-    {
-        return adoptRef(*new UserScript(WTFMove(userScript), world));
-    }
+static WTF::URL generateUniqueURL()
+{
+    static uint64_t identifier;
+    return { { }, makeString("user-script:", ++identifier) };
+}
 
-    UserScript(WebCore::UserScript, API::ContentWorld&);
+UserScript::UserScript(String&& source, URL&& url, Vector<String>&& allowlist, Vector<String>&& blocklist, UserScriptInjectionTime injectionTime, UserContentInjectedFrames injectedFrames, WaitForNotificationBeforeInjecting waitForNotification)
+    : m_source(WTFMove(source))
+    , m_url(url.isEmpty() ? generateUniqueURL() : WTFMove(url))
+    , m_allowlist(WTFMove(allowlist))
+    , m_blocklist(WTFMove(blocklist))
+    , m_injectionTime(injectionTime)
+    , m_injectedFrames(injectedFrames)
+    , m_waitForNotificationBeforeInjecting(waitForNotification)
+{
+}
 
-    WebCore::UserScript& userScript() { return m_userScript; }
-    const WebCore::UserScript& userScript() const { return m_userScript; }
-
-    ContentWorld& contentWorld() { return m_world; }
-    const ContentWorld& contentWorld() const { return m_world; }
-    
-private:
-    WebCore::UserScript m_userScript;
-    Ref<ContentWorld> m_world;
-};
-
-} // namespace API
+} // namespace WebCore

Source/WebCore/page/UserStyleSheet.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Apple Inc. All rights reserved.
+ * Copyright (C) 2024 Igalia S.L. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -23,34 +23,28 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#pragma once
+#include "config.h"
+#include "UserStyleSheet.h"
 
-#include "APIContentWorld.h"
-#include "APIObject.h"
-#include "UserScriptIdentifier.h"
-#include <WebCore/UserScript.h>
-#include <wtf/Identified.h>
+#include <wtf/text/StringConcatenateNumbers.h>
 
-namespace API {
+namespace WebCore {
 
-class UserScript final : public ObjectImpl<Object::Type::UserScript>, public Identified<WebKit::UserScriptIdentifier> {
-public:
-    static Ref<UserScript> create(WebCore::UserScript&& userScript, API::ContentWorld& world)
-    {
-        return adoptRef(*new UserScript(WTFMove(userScript), world));
-    }
+static WTF::URL generateUniqueURL()
+{
+    static uint64_t identifier;
+    return { { }, makeString("user-style:", ++identifier) };
+}
 
-    UserScript(WebCore::UserScript, API::ContentWorld&);
+UserStyleSheet::UserStyleSheet(const String& source, const URL& url, Vector<String>&& allowlist, Vector<String>&& blocklist, UserContentInjectedFrames injectedFrames, UserStyleLevel level, std::optional<PageIdentifier> pageID)
+    : m_source(source)
+    , m_url(url.isEmpty() ? generateUniqueURL() : url)
+    , m_allowlist(WTFMove(allowlist))
+    , m_blocklist(WTFMove(blocklist))
+    , m_injectedFrames(injectedFrames)
+    , m_level(level)
+    , m_pageID(pageID)
+{
+}
 
-    WebCore::UserScript& userScript() { return m_userScript; }
-    const WebCore::UserScript& userScript() const { return m_userScript; }
-
-    ContentWorld& contentWorld() { return m_world; }
-    const ContentWorld& contentWorld() const { return m_world; }
-    
-private:
-    WebCore::UserScript m_userScript;
-    Ref<ContentWorld> m_world;
-};
-
-} // namespace API
+} // namespace WebCore

q66/WPEWebKit

Source/WebCore/page/UserScript.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Apple Inc. All rights reserved.
+ * Copyright (C) 2024 Igalia S.L. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -23,32 +23,28 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#pragma once
+#include "config.h"
+#include "UserScript.h"
 
-#include "APIContentWorld.h"
-#include "APIObject.h"
-#include <WebCore/UserStyleSheet.h>
-#include <wtf/Identified.h>
+#include <wtf/text/StringConcatenateNumbers.h>
 
-namespace API {
+namespace WebCore {
 
-class UserStyleSheet final : public ObjectImpl<Object::Type::UserStyleSheet>, public Identified<UserStyleSheet> {
-public:
-    static Ref<UserStyleSheet> create(WebCore::UserStyleSheet userStyleSheet, API::ContentWorld& world)
-    {
-        return adoptRef(*new UserStyleSheet(WTFMove(userStyleSheet), world));
-    }
+static WTF::URL generateUserScriptUniqueURL()
+{
+    static uint64_t identifier;
+    return { { }, makeString("user-script:", ++identifier) };
+}
 
-    UserStyleSheet(WebCore::UserStyleSheet, API::ContentWorld&);
+UserScript::UserScript(String&& source, URL&& url, Vector<String>&& allowlist, Vector<String>&& blocklist, UserScriptInjectionTime injectionTime, UserContentInjectedFrames injectedFrames, WaitForNotificationBeforeInjecting waitForNotification)
+    : m_source(WTFMove(source))
+    , m_url(url.isEmpty() ? generateUserScriptUniqueURL() : WTFMove(url))
+    , m_allowlist(WTFMove(allowlist))
+    , m_blocklist(WTFMove(blocklist))
+    , m_injectionTime(injectionTime)
+    , m_injectedFrames(injectedFrames)
+    , m_waitForNotificationBeforeInjecting(waitForNotification)
+{
+}
 
-    const WebCore::UserStyleSheet& userStyleSheet() const { return m_userStyleSheet; }
-
-    ContentWorld& contentWorld() { return m_world; }
-    const ContentWorld& contentWorld() const { return m_world; }
-
-private:
-    WebCore::UserStyleSheet m_userStyleSheet;
-    Ref<ContentWorld> m_world;
-};
-
-} // namespace API
+} // namespace WebCore

Source/WebCore/page/UserStyleSheet.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Apple Inc. All rights reserved.
+ * Copyright (C) 2024 Igalia S.L. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -23,32 +23,28 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#pragma once
+#include "config.h"
+#include "UserStyleSheet.h"
 
-#include "APIContentWorld.h"
-#include "APIObject.h"
-#include <WebCore/UserStyleSheet.h>
-#include <wtf/Identified.h>
+#include <wtf/text/StringConcatenateNumbers.h>
 
-namespace API {
+namespace WebCore {
 
-class UserStyleSheet final : public ObjectImpl<Object::Type::UserStyleSheet>, public Identified<UserStyleSheet> {
-public:
-    static Ref<UserStyleSheet> create(WebCore::UserStyleSheet userStyleSheet, API::ContentWorld& world)
-    {
-        return adoptRef(*new UserStyleSheet(WTFMove(userStyleSheet), world));
-    }
+static WTF::URL generateUserStyleUniqueURL()
+{
+    static uint64_t identifier;
+    return { { }, makeString("user-style:", ++identifier) };
+}
 
-    UserStyleSheet(WebCore::UserStyleSheet, API::ContentWorld&);
+UserStyleSheet::UserStyleSheet(const String& source, const URL& url, Vector<String>&& allowlist, Vector<String>&& blocklist, UserContentInjectedFrames injectedFrames, UserStyleLevel level, std::optional<PageIdentifier> pageID)
+    : m_source(source)
+    , m_url(url.isEmpty() ? generateUserStyleUniqueURL() : url)
+    , m_allowlist(WTFMove(allowlist))
+    , m_blocklist(WTFMove(blocklist))
+    , m_injectedFrames(injectedFrames)
+    , m_level(level)
+    , m_pageID(pageID)
+{
+}
 
-    const WebCore::UserStyleSheet& userStyleSheet() const { return m_userStyleSheet; }
-
-    ContentWorld& contentWorld() { return m_world; }
-    const ContentWorld& contentWorld() const { return m_world; }
-
-private:
-    WebCore::UserStyleSheet m_userStyleSheet;
-    Ref<ContentWorld> m_world;
-};
-
-} // namespace API
+} // namespace WebCore
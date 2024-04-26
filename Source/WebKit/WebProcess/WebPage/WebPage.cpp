d675935cf878a8cc0c4fcf197ea31bf470bc6720
dkolesa <dkolesa@igalia.com>
2024-04-26 13:22:43 +0200

    Fix remaining !ENABLE(VIDEO) build failures https://bugs.webkit.org/show_bug.cgi?id=273314
    Reviewed by NOBODY (OOPS!).
    
    Recent refactorings introduced the breakages. They were fixed
    partially in ea2be408f27eb1009bbde7b0a2496018d4ca5e86 but not
    entirely.
    
    Notably, a misplacement of an #endif in WebCoreArgumentCoders
    resulted in strange IPC errors.
    
    * Source/WebCore/Modules/WebGPU/GPUDevice.cpp:
    * Source/WebCore/Modules/WebGPU/GPUDevice.h:
    * Source/WebCore/page/InteractionRegion.cpp:
    (WebCore::interactionRegionForRenderedRegion):
    * Source/WebCore/page/Page.cpp:
    (WebCore::Page::shouldBlockLayerTreeFreezingForVideo):
    * Source/WebCore/platform/graphics/MediaResourceSniffer.cpp:
    * Source/WebCore/platform/graphics/MediaResourceSniffer.h:
    * Source/WebKit/Shared/WebCoreArgumentCoders.serialization.in:
    * Source/WebKit/WebProcess/WebPage/WebPage.cpp:
    (WebKit::WebPage::startObservingNowPlayingMetadata):
    (WebKit::WebPage::stopObservingNowPlayingMetadata):

--- Source/WebKit/WebProcess/WebPage/WebPage.cpp
+++ Source/WebKit/WebProcess/WebPage/WebPage.cpp
@@ -9569,6 +9569,7 @@ void WebPage::setDefaultSpatialTrackingLabel(const String& label)
 
 void WebPage::startObservingNowPlayingMetadata()
 {
+#if ENABLE(VIDEO) || ENABLE(WEB_AUDIO)
     if (m_nowPlayingMetadataObserver)
         return;
 
@@ -9578,15 +9579,18 @@ void WebPage::startObservingNowPlayingMetadata()
     });
 
     WebCore::PlatformMediaSessionManager::sharedManager().addNowPlayingMetadataObserver(*m_nowPlayingMetadataObserver);
+#endif
 }
 
 void WebPage::stopObservingNowPlayingMetadata()
 {
+#if ENABLE(VIDEO) || ENABLE(WEB_AUDIO)
     auto nowPlayingMetadataObserver = std::exchange(m_nowPlayingMetadataObserver, nullptr);
     if (!nowPlayingMetadataObserver)
         return;
 
     WebCore::PlatformMediaSessionManager::sharedManager().removeNowPlayingMetadataObserver(*nowPlayingMetadataObserver);
+#endif
 }
 
 void WebPage::didAdjustVisibilityWithSelectors(Vector<String>&& selectors)
